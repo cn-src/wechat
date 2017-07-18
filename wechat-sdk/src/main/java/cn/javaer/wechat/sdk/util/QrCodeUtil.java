bf57f921f315d97d48d9eb87afd900528e8b893b
zhangpeng <java@javaer.cn>
2017-07-18 15:55:37 +0800

    update copyright

--- wechat-sdk/src/main/java/cn/javaer/wechat/sdk/util/QrCodeUtil.java
+++ wechat-sdk/src/main/java/cn/javaer/wechat/sdk/util/QrCodeUtil.java
@@ -1,3 +1,19 @@
+/*
+ *    Copyright 2017 zhangpeng
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
 package cn.javaer.wechat.sdk.util;
 
 import net.glxn.qrgen.javase.QRCode;
@@ -10,6 +26,6 @@ public class QrCodeUtil
 {
     public static String toHtmlImgBase64(final String str)
     {
-        return "data:image/jpg;base64,"+Base64.encodeBase64String(QRCode.from(str).withSize(300,300).stream().toByteArray());
+        return "data:image/jpg;base64," + Base64.encodeBase64String(QRCode.from(str).withSize(300, 300).stream().toByteArray());
     }
 }
