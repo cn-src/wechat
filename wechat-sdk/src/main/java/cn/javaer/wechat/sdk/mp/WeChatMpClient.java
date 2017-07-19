333364079920979a5484fc6d7d610e02ef0b62e9
zhangpeng <java@javaer.cn>
2017-07-19 16:06:13 +0800

    微信公众号网页授权开发

---
+++ wechat-sdk/src/main/java/cn/javaer/wechat/sdk/mp/WeChatMpClient.java
@@ -0,0 +1,29 @@
+/*
+ *    Copyright 2017 zhangpeng
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package cn.javaer.wechat.sdk.mp;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+
+public interface WeChatMpClient
+{
+    String baseUrl = "https://api.weixin.qq.com";
+    
+    @GET("/sns/oauth2/access_token")
+    Call<WeChatMpAccessTokenResponse> snsOauth2AccessToken(@Body WeChatMpAccessTokenRequest request);
+}
