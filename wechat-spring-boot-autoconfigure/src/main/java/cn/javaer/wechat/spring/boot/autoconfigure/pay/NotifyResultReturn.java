a6591a3a3350629cd9d7a2528fd94c1b8ac615b2
zhangpeng <java@javaer.cn>
2017-07-19 18:06:06 +0800

    重构微信支付，采用spring事件模型处理支付结果通知

--- wechat-spring-boot-autoconfigure/src/main/java/cn/javaer/wechat/spring/boot/autoconfigure/pay/NotifyResultReturn.java
+++ wechat-spring-boot-autoconfigure/src/main/java/cn/javaer/wechat/spring/boot/autoconfigure/pay/NotifyResultReturn.java
@@ -16,15 +16,30 @@
 
 package cn.javaer.wechat.spring.boot.autoconfigure.pay;
 
-import cn.javaer.wechat.sdk.pay.WeChatPayNotifyResult;
+import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NonNull;
+import lombok.NoArgsConstructor;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
 
 /**
  * @author zhangpeng
  */
 @Data
-public class NotifyResultEvent
+@NoArgsConstructor
+@AllArgsConstructor
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlRootElement(name = "xml")
+public class NotifyResultReturn
 {
-    @NonNull private WeChatPayNotifyResult notifyResult;
+    public static final NotifyResultReturn SUCCESS = new NotifyResultReturn("SUCCESS", "OK");
+    
+    @XmlElement(name = "return_code")
+    private String returnCode;
+    
+    @XmlElement(name = "return_msg")
+    private String returnMsg;
 }
---
+++ wechat-spring-boot-autoconfigure/src/main/java/cn/javaer/wechat/spring/boot/autoconfigure/pay/WeChatPayController.java
@@ -0,0 +1,44 @@
+/*
+ *    Copyright 2017 zhangpeng
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package cn.javaer.wechat.spring.boot.autoconfigure.pay;
+
+import cn.javaer.wechat.sdk.pay.WeChatPayNotifyResult;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author zhangpeng
+ */
+@RestController
+@RequestMapping("api/wechat/pay")   //TODO 可配置
+public class WeChatPayController
+{
+    private final ApplicationEventPublisher publisher;
+    
+    public WeChatPayController(final ApplicationEventPublisher publisher) {this.publisher = publisher;}
+    
+    //TODO 可配置
+    @RequestMapping(path = "notify_result", consumes = MediaType.TEXT_XML_VALUE, produces = MediaType.TEXT_XML_VALUE)
+    public NotifyResultReturn notifyResult(@RequestBody final WeChatPayNotifyResult notifyResult)
+    {
+        this.publisher.publishEvent(new NotifyResultEvent(notifyResult));
+        return NotifyResultReturn.SUCCESS;
+    }
+}
