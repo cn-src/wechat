7883f1ad54f1292c19d981bc8c2680ea2d37abab
zhangpeng <java@javaer.cn>
2017-07-19 17:20:15 +0800

    微信公众号网页授权开发

--- wechat-sdk/src/main/java/cn/javaer/wechat/sdk/mp/WeChatMpUtils.java
+++ wechat-sdk/src/main/java/cn/javaer/wechat/sdk/mp/WeChatMpUtils.java
@@ -16,9 +16,53 @@
 
 package cn.javaer.wechat.sdk.mp;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.net.URLEncoder;
+
 /**
  * @author zhangpeng
  */
 public class WeChatMpUtils
 {
+    /**
+     * 微信网页授权，生成授权url。<a href="https://mp.weixin.qq.com/wiki?t=resource/res_main&id=mp1421140842">官方文档<a/>
+     * <p>
+     * 用户在微信客户端中访问第三方网页，公众号可以通过微信网页授权机制，来获取用户基本信息，进而实现业务逻辑。
+     *
+     * @param appid       公众号的唯一标识
+     * @param redirectUri 授权后重定向的回调链接地址
+     * @param scope       应用授权作用域，snsapi_base（不弹出授权页面，直接跳转，只能获取用户openid），snsapi_userinfo （弹出授权页面，可通过openid拿到昵称、性别、所在地。并且，即使在未关注的情况下，只要用户授权，也能获取其信息）
+     * @param state       重定向后会带上state参数，开发者可以填写a-zA-Z0-9的参数值，最多128字节
+     * @return 样例：
+     * <p> scope为snsapi_base
+     * <br/> https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx520c15f417810387&redirect_uri=https%3A%2F%2Fchong.qq.com%2Fphp%2Findex.php%3Fd%3D%26c%3DwxAdapter%26m%3DmobileDeal%26showwxpaytitle%3D1%26vb2ctag%3D4_2030_5_1194_60&response_type=code&scope=snsapi_base&state=123#wechat_redirect
+     * <p> scope为snsapi_userinfo
+     * <br/> https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxf0e81c3bee622d60&redirect_uri=http%3A%2F%2Fnba.bluewebgame.com%2Foauth_response.php&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect
+     */
+    public static String generateAuthorizeUrl(
+        @NotNull final String appid,
+        @NotNull final String redirectUri,
+        @NotNull final AuthorizeScope scope,
+        @NotNull final String state)
+    {
+        return "https://open.weixin.qq.com/connect/oauth2/authorize?appid=" + appid
+               + "&redirect_uri=" + URLEncoder.encode(redirectUri)
+               + "&response_type=code&scope=" + scope.getScope()
+               + "&state=" + state + "#wechat_redirect";
+    }
+    
+    /**
+     * @see #generateAuthorizeUrl(String, String, AuthorizeScope, String)
+     */
+    public static String generateAuthorizeUrl(
+        @NotNull final String appid,
+        @NotNull final String redirectUri,
+        @NotNull final AuthorizeScope scope)
+    {
+        return "https://open.weixin.qq.com/connect/oauth2/authorize?appid=" + appid
+               + "&redirect_uri=" + URLEncoder.encode(redirectUri)
+               + "&response_type=code&scope=" + scope.getScope()
+               + "#wechat_redirect";
+    }
 }
