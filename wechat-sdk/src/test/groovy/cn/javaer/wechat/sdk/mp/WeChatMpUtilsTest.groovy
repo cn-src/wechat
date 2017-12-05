a63ed3e61719655cd4dbbeb3fa463ab9969b384b
zhangpeng <710692586@qq.com>
2017-12-05 10:59:00 +0800

    reformat

--- wechat-sdk/src/test/groovy/cn/javaer/wechat/sdk/mp/WeChatMpUtilsTest.groovy
+++ wechat-sdk/src/test/groovy/cn/javaer/wechat/sdk/mp/WeChatMpUtilsTest.groovy
@@ -42,9 +42,9 @@ class WeChatMpUtilsTest extends Specification {
         when:
         final String authorizeUrl = WeChatMpUtils.generateAuthorizeUrl("wx520c15f417810387",
                 "https://chong.qq.com/php/index.php?d=&c=wxAdapter&m=mobileDeal&showwxpaytitle=1&vb2ctag=4_2030_5_1194_60",
-                AuthorizeScope.BASE);
+                AuthorizeScope.BASE)
         and:
-        final String expected = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx520c15f417810387&redirect_uri=https%3A%2F%2Fchong.qq.com%2Fphp%2Findex.php%3Fd%3D%26c%3DwxAdapter%26m%3DmobileDeal%26showwxpaytitle%3D1%26vb2ctag%3D4_2030_5_1194_60&response_type=code&scope=snsapi_base#wechat_redirect";
+        final String expected = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx520c15f417810387&redirect_uri=https%3A%2F%2Fchong.qq.com%2Fphp%2Findex.php%3Fd%3D%26c%3DwxAdapter%26m%3DmobileDeal%26showwxpaytitle%3D1%26vb2ctag%3D4_2030_5_1194_60&response_type=code&scope=snsapi_base#wechat_redirect"
 
         then:
         expected == authorizeUrl
