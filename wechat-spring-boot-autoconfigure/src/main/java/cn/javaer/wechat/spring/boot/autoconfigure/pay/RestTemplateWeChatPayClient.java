f97c7e4341edad008e91eec9b62633d85feebad4
zhangpeng <710692586@qq.com>
2017-12-05 16:41:37 +0800

    refactoring

--- wechat-spring-boot-autoconfigure/src/main/java/cn/javaer/wechat/spring/boot/autoconfigure/pay/RestTemplateWeChatPayClient.java
+++ wechat-spring-boot-autoconfigure/src/main/java/cn/javaer/wechat/spring/boot/autoconfigure/pay/RestTemplateWeChatPayClient.java
@@ -4,13 +4,35 @@ import cn.javaer.wechat.sdk.pay.WeChatPayClient;
 import cn.javaer.wechat.sdk.pay.WeChatPayException;
 import cn.javaer.wechat.sdk.pay.WeChatPayUnifiedOrderRequest;
 import cn.javaer.wechat.sdk.pay.WeChatPayUnifiedOrderResponse;
+import cn.javaer.wechat.sdk.util.WeChatUtils;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * @author zhangpeng
  */
 public class RestTemplateWeChatPayClient implements WeChatPayClient {
+
+    private final RestTemplate restTemplate;
+
+    public RestTemplateWeChatPayClient(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
     @Override
     public WeChatPayUnifiedOrderResponse unifiedOrder(WeChatPayUnifiedOrderRequest request) throws WeChatPayException {
-        return null;
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_XML);
+        HttpEntity<WeChatPayUnifiedOrderRequest> httpEntity = new HttpEntity<>(request, headers);
+        String url = WeChatUtils.joinPath(WeChatPayClient.BASE_URL, WeChatPayClient.UNIFIED_ORDER_URL);
+        ResponseEntity<WeChatPayUnifiedOrderResponse> responseEntity = restTemplate.postForEntity(
+                url,
+                httpEntity,
+                WeChatPayUnifiedOrderResponse.class);
+
+        return responseEntity.getBody();
     }
 }
