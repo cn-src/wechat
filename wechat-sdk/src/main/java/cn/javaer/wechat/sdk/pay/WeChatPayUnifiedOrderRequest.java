205731e17575744fbb69ebc1606a4115644ed54c
zhangpeng <710692586@qq.com>
2017-12-05 14:43:28 +0800

    add comments

--- wechat-sdk/src/main/java/cn/javaer/wechat/sdk/pay/WeChatPayUnifiedOrderRequest.java
+++ wechat-sdk/src/main/java/cn/javaer/wechat/sdk/pay/WeChatPayUnifiedOrderRequest.java
@@ -38,78 +38,154 @@ import javax.xml.bind.annotation.XmlRootElement;
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlRootElement(name = "xml")
 public class WeChatPayUnifiedOrderRequest {
+
+    /**
+     * 公众账号ID
+     */
     @XmlElement(name = "appid")
     private String appid;
 
+    /**
+     * 商户号
+     */
     @XmlElement(name = "mch_id")
     private String mchId;
 
+    /**
+     * 子商户公众账号ID
+     */
     @XmlElement(name = "sub_appid")
     private String subAppId;
 
+    /**
+     * 子商户号
+     */
     @XmlElement(name = "sub_mch_id")
     private String subMchId;
 
+    /**
+     * 设备号
+     */
     @XmlElement(name = "device_info")
     private String deviceInfo;
 
+    /**
+     * 随机字符串
+     */
     @XmlElement(name = "nonce_str")
     private String nonceStr;
 
+    /**
+     * 签名
+     */
     @XmlElement(name = "sign")
     private String sign;
 
+    /**
+     * 签名类型
+     */
     @XmlElement(name = "sign_type")
     private String signType;
 
+    /**
+     * 商品描述
+     */
     @XmlElement(name = "body")
     private String body;
 
+    /**
+     * 商品详情
+     */
     @XmlElement(name = "detail")
     private String detail;
 
+    /**
+     * 附加数据
+     */
     @XmlElement(name = "attach")
     private String attach;
 
+    /**
+     * 商户订单号
+     */
     @XmlElement(name = "out_trade_no")
     private String outTradeNo;
 
+    /**
+     * 货币类型
+     */
     @XmlElement(name = "fee_type")
     private String feeType;
 
+    /**
+     * 总金额
+     */
     @XmlElement(name = "total_fee")
     private Integer totalFee;
 
+    /**
+     * 终端IP
+     */
     @XmlElement(name = "spbill_create_ip")
     private String spbillCreateIp;
 
+    /**
+     * 交易起始时间
+     */
     @XmlElement(name = "time_start")
     private String timeStart;
 
+    /**
+     * 交易结束时间
+     */
     @XmlElement(name = "time_expire")
     private String timeExpire;
 
+    /**
+     * 订单优惠标记
+     */
     @XmlElement(name = "goods_tag")
     private String goodsTag;
 
+    /**
+     * 通知地址
+     */
     @XmlElement(name = "notify_url")
     private String notifyUrl;
 
+    /**
+     * 交易类型
+     */
     @XmlElement(name = "trade_type")
     private String tradeType;
 
+    /**
+     * 商品ID
+     */
     @XmlElement(name = "product_id")
     private String productId;
 
+    /**
+     * 指定支付方式
+     */
     @XmlElement(name = "limit_pay")
     private String limitPay;
 
+    /**
+     * 用户标识
+     */
     @XmlElement(name = "openid")
     private String openid;
 
+    /**
+     * 用户子标识
+     */
     @XmlElement(name = "sub_openid")
     private String subOpenid;
 
+    /**
+     * 场景信息
+     */
     @XmlElement(name = "scene_info")
     private String sceneInfo;
 }
