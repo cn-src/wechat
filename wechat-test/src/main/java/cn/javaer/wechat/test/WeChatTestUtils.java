7be38021ae645cf0e783b009c5415f4159f010cd
zhangpeng <710692586@qq.com>
2017-12-07 16:17:22 +0800

    WeChatTestUtils for test

--- wechat-test/src/main/java/cn/javaer/wechat/test/WeChatTestUtils.java
+++ wechat-test/src/main/java/cn/javaer/wechat/test/WeChatTestUtils.java
@@ -3,21 +3,33 @@ package cn.javaer.wechat.test;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
+import java.io.IOException;
 import java.io.StringReader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 /**
  * @author zhangpeng
  */
-public class JaxbUtils {
+public class WeChatTestUtils {
 
     @SuppressWarnings("unchecked")
-    public static <T> T unmarshal(String xmlStr, Class<T> clazz) {
+    public static <T> T jaxbUnmarshal(String xmlStr, Class<T> clazz) {
         try {
             JAXBContext context = JAXBContext.newInstance(clazz);
             Unmarshaller unmarshaller = context.createUnmarshaller();
             StringReader reader = new StringReader(xmlStr);
             return (T) unmarshaller.unmarshal(reader);
         } catch (JAXBException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static String readClassPathFileToUTFString(String fileClassPath, Class clazzForClassPath) {
+        try {
+            final byte[] bytes = Files.readAllBytes(Paths.get(clazzForClassPath.getResource(fileClassPath).getFile()));
+            return new String(bytes, "UTF-8");
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
