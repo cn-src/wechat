b88a9e4cba90b17be181210a1bd5ad728969b24d
zhangpeng <710692586@qq.com>
2017-12-11 15:44:52 +0800

    add test case

--- wechat-sdk/src/test/java/cn/javaer/wechat/sdk/pay/WeChatPayUtilsTest.java
+++ wechat-sdk/src/test/java/cn/javaer/wechat/sdk/pay/WeChatPayUtilsTest.java
@@ -1,6 +1,7 @@
 package cn.javaer.wechat.sdk.pay;
 
 import cn.javaer.wechat.sdk.pay.model.WeChatPayUnifiedOrderRequest;
+import cn.javaer.wechat.sdk.pay.model.WeChatPayUnifiedOrderResponse;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -11,7 +12,7 @@ import static org.junit.Assert.assertEquals;
 public class WeChatPayUtilsTest {
 
     @Test
-    public void sign() {
+    public void sign1() {
         final WeChatPayUnifiedOrderRequest request = WeChatPayUnifiedOrderRequest.builder()
                 .body("body")
                 .nonceStr("nonceStr")
@@ -29,5 +30,22 @@ public class WeChatPayUtilsTest {
         request.setSign(WeChatPayUtils.sign(request, "key"));
 
        assertEquals("EFAF8A5CD089B3749AF9EAEE6C6B4156",request.getSign());
+    }
+
+    @Test
+    public void sign2() {
+        final WeChatPayUnifiedOrderResponse response = new WeChatPayUnifiedOrderResponse();
+        response.setReturnCode("SUCCESS");
+        response.setReturnMsg("OK");
+        response.setAppid("wx2421b1c4370ec43b");
+        response.setMchId("10000100");
+        response.setNonceStr("IITRi8Iabbblz1Jc");
+        response.setResultCode("SUCCESS");
+        response.setPrepayId("wx201411101639507cbf6ffd8b0779950874");
+        response.setTradeType("JSAPI");
+
+        response.setSign(WeChatPayUtils.sign(response,"key"));
+
+        assertEquals("BC884153761883FE608EA956BD05A6F5",response.getSign());
     }
 }
