38191a713e66ba4cac98c7cdbbaf39b7056d2572
zhangpeng <710692586@qq.com>
2017-12-12 17:11:33 +0800

    update javadoc & reformat

--- wechat-spring-boot-autoconfigure/src/main/java/cn/javaer/wechat/spring/boot/autoconfigure/pay/WeChatPayProperties.java
+++ wechat-spring-boot-autoconfigure/src/main/java/cn/javaer/wechat/spring/boot/autoconfigure/pay/WeChatPayProperties.java
@@ -27,7 +27,7 @@ import org.springframework.core.env.Environment;
 import org.springframework.util.StringUtils;
 
 /**
- * 微信支付-配置项
+ * 微信支付-配置.
  *
  * @author zhangpeng
  */
@@ -40,39 +40,39 @@ public class WeChatPayProperties implements EnvironmentAware, InitializingBean {
 
 
     /**
-     * 公众号的唯一标识
+     * 公众号的唯一标识.
      */
     @NonNull
     private String appId;
 
     /**
-     * 商户号
+     * 商户号.
      */
     @NonNull
     private String mchId;
 
     /**
-     * 商户的key，需要去微信支付平台设置
+     * 商户的key, 需要去微信支付平台设置.
      */
     @NonNull
     private String mchKey;
 
     /**
-     * 接收微信异步通知消息的地址
-     * <p>
+     * 接收微信异步通知消息的地址.
+     * <br>
      * 如：http://www.demo.com:8080
      */
     @NonNull
     private String notifyAddress;
 
     /**
-     * 服务器的公网ip
+     * 服务器的公网ip.
      */
     @NonNull
     private String clientIp;
 
     /**
-     * 接收支付结果通知 Controller 的 path
+     * 接收支付结果通知 Controller 的 path.
      */
     private String notifyResultPath = NOTIFY_RESULT_PATH;
 
