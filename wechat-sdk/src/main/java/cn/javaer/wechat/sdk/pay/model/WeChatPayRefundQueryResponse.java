c6deab5c514641427d53155efc0fdb921718d0f5
zhangpeng <710692586@qq.com>
2017-12-18 14:36:24 +0800

    dev

--- wechat-sdk/src/main/java/cn/javaer/wechat/sdk/pay/model/WeChatPayRefundQueryResponse.java
+++ wechat-sdk/src/main/java/cn/javaer/wechat/sdk/pay/model/WeChatPayRefundQueryResponse.java
@@ -69,6 +69,11 @@ public class WeChatPayRefundQueryResponse extends WeChatPayResponse {
     @SignIgnore
     private Map<String, WeChatRefund> refundMap;
 
+    /**
+     * 获取退款记录信息.
+     *
+     * @return <code>Map&lt;String, WeChatRefund&gt;</code>
+     */
     public Map<String, WeChatRefund> getRefundMap() {
 
         if (null == this.refundMap && null != this.otherMap) {
@@ -78,17 +83,29 @@ public class WeChatPayRefundQueryResponse extends WeChatPayResponse {
             mappingMap.put("refund_channel_", (val, coupon) -> coupon.setRefundChannel(val));
             mappingMap.put("refund_fee_", (val, coupon) -> coupon.setRefundFee(Integer.valueOf(val)));
             mappingMap.put("settlement_refund_fee_", (val, coupon) -> coupon.setSettlementRefundFee(Integer.valueOf(val)));
-//            mappingMap.put("coupon_type_", (val, coupon) -> coupon.setFee(val));
             mappingMap.put("coupon_refund_fee_", (val, coupon) -> coupon.setCouponRefundFee(Integer.valueOf(val)));
             mappingMap.put("coupon_refund_count_", (val, coupon) -> coupon.setCouponRefundCount(Integer.valueOf(val)));
-//            mappingMap.put("coupon_refund_id_", (val, coupon) -> coupon.setFee(val));
-//            mappingMap.put("coupon_refund_fee_", (val, coupon) -> coupon.setFee(val));
             mappingMap.put("refund_status_", (val, coupon) -> coupon.setRefundStatus(val));
             mappingMap.put("refund_account_", (val, coupon) -> coupon.setRefundAccount(val));
             mappingMap.put("refund_recv_accout_", (val, coupon) -> coupon.setRefundRecvAccout(val));
             mappingMap.put("refund_success_time_", (val, coupon) -> coupon.setRefundSuccessTime(val));
+
             this.refundMap = WeChatPayUtils.dynamicMapping(this.otherMap, mappingMap, WeChatRefund::new);
 
+            for (final Map.Entry<String, WeChatRefund> entry : this.refundMap.entrySet()) {
+                final String key = entry.getKey();
+                final WeChatRefund refund = entry.getValue();
+                if (null == refund.getRefundCoupons()) {
+                    refund.setRefundCoupons(new TreeMap<>());
+                }
+
+                final Map<String, BiConsumer<String, WeChatPayCoupon>> mappingMap2 = new TreeMap<>();
+                mappingMap2.put("coupon_type_" + key + "_", (val, coupon) -> coupon.setType(WeChatPayCoupon.Type.valueOf(val)));
+                mappingMap2.put("coupon_refund_id_" + key + "_", (val, coupon) -> coupon.setId(val));
+                mappingMap2.put("coupon_refund_fee_" + key + "_", (val, coupon) -> coupon.setFee(Integer.valueOf(val)));
+
+                WeChatPayUtils.dynamicMapping(this.otherMap, mappingMap2, WeChatPayCoupon::new);
+            }
         }
         return this.refundMap;
     }
