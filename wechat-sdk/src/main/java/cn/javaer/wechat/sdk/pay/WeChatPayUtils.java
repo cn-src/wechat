d09bacf864d9004ca919cf16e8e644a1db72554f
zhangpeng <710692586@qq.com>
2017-12-20 17:21:37 +0800

    fixed: toUpperCase(Locale.ENGLISH)

--- wechat-sdk/src/main/java/cn/javaer/wechat/sdk/pay/WeChatPayUtils.java
+++ wechat-sdk/src/main/java/cn/javaer/wechat/sdk/pay/WeChatPayUtils.java
@@ -31,6 +31,7 @@ import java.lang.reflect.Field;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.TreeMap;
 import java.util.concurrent.ConcurrentHashMap;
@@ -100,7 +101,7 @@ public class WeChatPayUtils {
             sb.append(entry.getKey()).append('=').append(entry.getValue()).append('&');
         }
         sb.append("key").append('=').append(key);
-        return DigestUtils.md5Hex(sb.toString()).toUpperCase();
+        return DigestUtils.md5Hex(sb.toString()).toUpperCase(Locale.ENGLISH);
     }
 
     /**
