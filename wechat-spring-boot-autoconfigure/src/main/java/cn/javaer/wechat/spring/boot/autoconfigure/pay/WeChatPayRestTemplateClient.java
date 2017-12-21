7a83cbbeb32334bc532f86bf0879fc9c819017db
zhangpeng <710692586@qq.com>
2017-12-21 14:28:00 +0800

    dev: 接口实现

--- wechat-spring-boot-autoconfigure/src/main/java/cn/javaer/wechat/spring/boot/autoconfigure/pay/WeChatPayRestTemplateClient.java
+++ wechat-spring-boot-autoconfigure/src/main/java/cn/javaer/wechat/spring/boot/autoconfigure/pay/WeChatPayRestTemplateClient.java
@@ -22,6 +22,10 @@ import cn.javaer.wechat.sdk.pay.model.WeChatPayCloseOrderRequest;
 import cn.javaer.wechat.sdk.pay.model.WeChatPayCloseOrderResponse;
 import cn.javaer.wechat.sdk.pay.model.WeChatPayOrderQueryRequest;
 import cn.javaer.wechat.sdk.pay.model.WeChatPayOrderQueryResponse;
+import cn.javaer.wechat.sdk.pay.model.WeChatPayRefundQueryRequest;
+import cn.javaer.wechat.sdk.pay.model.WeChatPayRefundQueryResponse;
+import cn.javaer.wechat.sdk.pay.model.WeChatPayRefundRequest;
+import cn.javaer.wechat.sdk.pay.model.WeChatPayRefundResponse;
 import cn.javaer.wechat.sdk.pay.model.WeChatPayUnifiedOrderRequest;
 import cn.javaer.wechat.sdk.pay.model.WeChatPayUnifiedOrderResponse;
 import cn.javaer.wechat.sdk.util.WeChatUtils;
@@ -42,35 +46,83 @@ public class WeChatPayRestTemplateClient implements WeChatPayClient {
 
     private final WeChatPayProperties weChatPayProperties;
 
-    public WeChatPayRestTemplateClient(final RestTemplate restTemplate, final WeChatPayProperties weChatPayProperties) {
+    /**
+     * Instantiates a new WeChatPayRestTemplateClient.
+     *
+     * @param restTemplate RestTemplate
+     * @param weChatPayProperties WeChatPayProperties
+     */
+    public WeChatPayRestTemplateClient(
+        final RestTemplate restTemplate, final WeChatPayProperties weChatPayProperties) {
+
         this.restTemplate = restTemplate;
         this.weChatPayProperties = weChatPayProperties;
     }
 
     @Override
-    public WeChatPayUnifiedOrderResponse unifiedOrder(final WeChatPayUnifiedOrderRequest request)
-            throws WeChatPayException {
+    public WeChatPayUnifiedOrderResponse unifiedOrder(
+        final WeChatPayUnifiedOrderRequest request) throws WeChatPayException {
+
+        return postForEntity(
+            WeChatPayClient.UNIFIED_ORDER_PATH,
+            request,
+            WeChatPayUnifiedOrderResponse.class)
+            .getBody();
+    }
+
+    @Override
+    public WeChatPayOrderQueryResponse orderQuery(
+        final WeChatPayOrderQueryRequest request) throws WeChatPayException {
+
+        return postForEntity(
+            WeChatPayClient.ORDER_QUERY_PATH,
+            request,
+            WeChatPayOrderQueryResponse.class)
+            .getBody();
+    }
+
+    @Override
+    public WeChatPayCloseOrderResponse closeOrder(
+        final WeChatPayCloseOrderRequest request) throws WeChatPayException {
+
+        return postForEntity(
+            WeChatPayClient.CLOSE_ORDER_PATH,
+            request,
+            WeChatPayCloseOrderResponse.class)
+            .getBody();
+    }
+
+    @Override
+    public WeChatPayRefundResponse refund(
+        final WeChatPayRefundRequest request) throws WeChatPayException {
+
+        return postForEntity(
+            WeChatPayClient.REFUND_PATH,
+            request,
+            WeChatPayRefundResponse.class)
+            .getBody();
+    }
+
+    @Override
+    public WeChatPayRefundQueryResponse refundQuery(
+        final WeChatPayRefundQueryRequest request) throws WeChatPayException {
+
+        return postForEntity(
+            WeChatPayClient.REFUND_QUERY_PATH,
+            request,
+            WeChatPayRefundQueryResponse.class)
+            .getBody();
+    }
+
+    private <Q, S> ResponseEntity<S> postForEntity(
+        final String apiPath, final Q request, final Class<S> responseClass) {
+
         final HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.TEXT_XML);
-        final HttpEntity<WeChatPayUnifiedOrderRequest> httpEntity = new HttpEntity<>(request, headers);
-        final String url = WeChatUtils.joinPath(
-                weChatPayProperties.getApiBasePath(), WeChatPayClient.UNIFIED_ORDER_PATH);
-        final ResponseEntity<WeChatPayUnifiedOrderResponse> responseEntity = restTemplate.postForEntity(
-                url,
-                httpEntity,
-                WeChatPayUnifiedOrderResponse.class);
-
-        return responseEntity.getBody();
-    }
-
-    @Override
-    public WeChatPayOrderQueryResponse orderQuery(final WeChatPayOrderQueryRequest request) throws WeChatPayException {
-        return null;
-    }
-
-    @Override
-    public WeChatPayCloseOrderResponse closeOrder(final WeChatPayCloseOrderRequest request) throws WeChatPayException {
-        // TODO
-        return null;
+        final HttpEntity<Q> httpEntity = new HttpEntity<>(request, headers);
+        return this.restTemplate.postForEntity(
+            WeChatUtils.joinPath(this.weChatPayProperties.getApiBasePath(), apiPath),
+            httpEntity,
+            responseClass);
     }
 }
